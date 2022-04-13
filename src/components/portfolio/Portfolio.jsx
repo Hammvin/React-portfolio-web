cd81e864bf057e8b5fa7c46905221d8d0690173d
Hammvin <hammondvin@gmail.com>
2022-04-13 09:32:48 +0300

    Portfolio Section Complete

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
 import './Portfolio.css'
+import MeWeb from '../../assets/meweb.jpg'
+import PORTWEB1 from '../../assets/portweb1.jpg'
+import PORTWEB2 from '../../assets/portweb2.png' 
+import MeWeb2 from '../../assets/meweb2.png'
 
+const images = [
+  {
+    id:1,
+    image: MeWeb,
+    title: 'Portfolio website',
+    github:'https://github.com/Hammvin',
+    demo:'https://www.vincenthammond.co.ke'
+  },
+  {
+    id: 2,
+    image: MeWeb2,
+    title: 'Portfolio website',
+    github: 'https://github.com/Hammvin',
+    demo: 'https://www.vincenthammond.co.ke'
+  },
+  {
+    id: 3,
+    image: PORTWEB1,
+    title: 'Sample website',
+    github: 'https://github.com/Hammvin',
+    demo: 'https://miamiproject.vincenthammond.co.ke'
+  },
+  {
+    id: 4,
+    image: PORTWEB2,
+    title: 'Sample website',
+    github: 'https://github.com/Hammvin',
+    demo: 'https://miamiproject.vincenthammond.co.ke'
+  }
+]
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Resent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          images.map(({id, image, title, github, demo}) =>{
+            return(
+              <article className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn'>GitHub</a>
+                  <a href={demo} className='btn btn-primary'>Live View</a>
+                </div>
+              </div>
+            </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,90 @@
 import React from 'react'
 import './Services.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <arcticle className="service">
+          <div className="service__head">
+            <h3>Web Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+          </ul>
+        </arcticle>
+
+        <arcticle className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+          </ul>
+        </arcticle>
+
+        <arcticle className="service">
+          <div className="service__head">
+            <h3>Search Engine Opt</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+            <li>
+            <BsFillPatchCheckFill className='services__list-icon'/>
+            <p>Lorem ipsum dolor sit amet elit.</p>
+            </li>
+          </ul>
+        </arcticle>
+      </div>
+    </section>
   )
 }
 
