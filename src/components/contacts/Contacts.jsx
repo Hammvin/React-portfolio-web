8fa223bd5fef2fd955230bd2a97f02b6460b8097
Hammvin <hammondvin@gmail.com>
2022-04-14 10:00:56 +0300

    Used map() to load Array data to the page both in Portfolio and Testimonials

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './Contacts.css'
+import {BiMailSend} from 'react-icons/bi'
+import {BsWhatsapp} from 'react-icons/bs'
+import {RiMessengerLine} from 'react-icons/ri'
 
 const Contacts = () => {
   return (
-    <section id='contacts'>Contacts</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <BiMailSend className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>hammond***@gmail.com</h5>
+            <a href="mailto:hammondvin@gmail.com" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Vin Hammond</h5>
+            <a href="https://m.me/vin.hammond" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+254***618046</h5>
+            <a href="https://api.whatsapp.com/send?phone=+254713618046" target="_blank">Send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="text" name='email' placeholder='Email Adress' required/>
+          <textarea name="" id="" cols="30" rows="10" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,76 @@
 import React from 'react'
 import './Testimonials.css'
+import CLIENT1 from '../../assets/client1.jpeg'
+import CLIENT2 from '../../assets/client2.jpg'
+import CLIENT3 from '../../assets/client3.jpg'
+import CLIENT4 from '../../assets/client4.jpg'
+import CLIENT5 from '../../assets/client5.jpg'
+//import {Pagination} from 'swiper';
+//import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+//import 'swiper/css';
+//import 'swiper/css/pagination';
+
+
+const clients = [
+  {
+    id: 1,
+    image: CLIENT1,
+    cname: 'Sally Monalisa',
+    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+  },
+  {
+    id: 2,
+    image: CLIENT2,
+    cname: 'Mary Mboya',
+    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+  },
+  {
+    id: 3,
+    image: CLIENT3,
+    cname: 'Alex Monta',
+    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+  },
+  {
+    id: 4,
+    image: CLIENT4,
+    cname: 'Tonny Adar',
+    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+  }
+]
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <article className="container testimonials__container" 
+       // install Swiper modules
+      // modules={[ Pagination ]}
+       //spaceBetween={40}
+       //slidesPerView={1}
+       //pagination={{ clickable: true }}
+      >
+        {
+          clients.map(({id, image, cname, review }) =>{
+            return(
+              <div className="testimonial">
+                <div className="client__avatar">
+                  <img src={image} alt="Avatar" />
+                </div>
+                <h5 className='client__name'>{cname}</h5>
+                <small className="client__review">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                  Sequi dolores consectetur culpa soluta nobis doloribus! 
+                  Tempore quo accusantium molestias doloremque.
+                </small>
+              </div>
+            )
+          })
+        }
+      </article>
+    </section>
   )
 }
 
