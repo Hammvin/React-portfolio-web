e47467e5c0901cab112f8979e6115ec97422e451
Hammvin <hammondvin@gmail.com>
2022-04-14 11:25:08 +0300

    Used usState hook to set Active Navs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import './Footer.css'
+import {BsFacebook} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+import {BsTwitter} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer_link'></a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contacts</a></li>
+      </ul>
+      
+      <div className="footer__socials">
+        <a href="https://facebook.com" target="_blank"><BsFacebook/></a>
+        <a href="https://instagram.com" target="_blank"><BsInstagram /></a>
+        <a href="https://twitter.com" target="_blank"><BsTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Vincent Hammond. All rights Reserved</small>
+      </div>
+
+    </footer>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,6 +5,8 @@ import {AiOutlineUserAdd} from 'react-icons/ai';
 import {BsFillJournalBookmarkFill} from 'react-icons/bs';
 import {MdHomeRepairService} from 'react-icons/md'
 import {MdOutlineContactPhone} from 'react-icons/md';
+import {MdWorkOutline} from 'react-icons/md'
+import {BsPeopleFill} from 'react-icons/bs'
 import { useState } from 'react';
 
 const Nav = () => {
@@ -15,6 +17,8 @@ const Nav = () => {
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUserAdd/></a>
       <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsFillJournalBookmarkFill/></a>
       <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdHomeRepairService/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><MdWorkOutline/></a>
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><BsPeopleFill/></a>
       <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlineContactPhone/></a>
     </nav>
   )
